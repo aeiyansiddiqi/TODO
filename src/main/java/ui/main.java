8569e5ab6d2fb534008a511bc84da6ee9d08b633
aeiyansiddiqi <155936059+aeiyansiddiqi@users.noreply.github.com>
2024-01-14 22:58:51 -0500

    Create main.java

---
+++ src/main/java/ui/main.java
@@ -0,0 +1,11 @@
+package ui;
+//import classes.screen;
+//package ui;
+import classes.screen;
+public class main {
+    
+    public static void main(String[] args) {
+        screen Screen = new screen();
+
+    }
+}
